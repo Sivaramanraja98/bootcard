aca877bca29654887c249563e8396cf30ab6d6b4
SIVARAMAN ALIAS RAJA S <127581183+Sivaramanraja98@users.noreply.github.com>
2023-07-27 21:08:09 +0530

    Add files via upload

---
+++ src/CardData.js
@@ -0,0 +1,121 @@
+const cardsData = [
+    {
+      title: 'Free',
+      price: '$ 0',
+      features: [
+        {
+          content: "Single User",
+          isAvail: true,
+        },
+        {
+          content: "5GB Storage",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Public Projects",
+          isAvail: true,
+        },
+        {
+          content: "Community Access",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Private Projects",
+          isAvail: false,
+        },
+        {
+          content: "Dedicated Phone Support",
+          isAvail: false,
+        },
+        {
+          content: "Free Subdomain",
+          isAvail: false,
+        },
+        {
+          content: "Monthly Status Reports",
+          isAvail: false,
+        },
+      ],
+    },
+    {
+      title: 'Plus',
+      price: '$ 9',
+      features: [
+        {
+          content: "5 Users",
+          isAvail: true,
+          Bold: true,
+        },
+        {
+          content: "50GB Storage",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Public Projects",
+          isAvail: true,
+        },
+        {
+          content: "Community Access",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Private Projects",
+          isAvail: true,
+        },
+        {
+          content: "Dedicated Phone Support",
+          isAvail: true,
+        },
+        {
+          content: "Free Subdomain",
+          isAvail: true,
+        },
+        {
+          content: "Monthly Status Reports",
+          isAvail: false,
+        },
+      ],
+    },
+    {
+      title: 'Pro',
+      price: '$ 49',
+      features: [
+        {
+          content: "Unlimited Users",
+          isAvail: true,
+          Bold: true,
+        },
+        {
+          content: "150GB Storage",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Public Projects",
+          isAvail: true,
+        },
+        {
+          content: "Community Access",
+          isAvail: true,
+        },
+        {
+          content: "Unlimited Private Projects",
+          isAvail: true,
+        },
+        {
+          content: "Dedicated Phone Support",
+          isAvail: true,
+        },
+        {
+          content: "Free Subdomain",
+          isAvail: true,
+          isunlimited: true,
+        },
+        {
+          content: "Monthly Status Reports",
+          isAvail: true,
+        },
+      ],
+    },
+  ];
+
+  export default cardsData;
